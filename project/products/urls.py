b3e58cdd1dc5bfa3a019f754950d09b82dcbc568
Ivan Nikitin <vanechka-nikitin-2004@mail.ru>
2023-02-19 21:19:56 +0800

    refactor: Add new path for pay

--- project/products/urls.py
+++ project/products/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import (
     HomeProductsView,
     ProductView,
+    CreateCheckoutSessionView,
 )
 
 
@@ -10,5 +11,5 @@ urlpatterns = [
     path('', HomeProductsView.as_view(), name='home-page'),
     # At the end, a slash is usually put, but according to the test it should be done like this
     path('item/<int:pk>', ProductView.as_view(), name='detail-product'),
-    path('buy/<int:pk>', name=''),
+    path('buy/<int:pk>', CreateCheckoutSessionView.as_view(), name='create-checkout-session'),
 ]
