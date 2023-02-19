6cbd81d7595b469c49f1e2ba2c34d37fe4669020
Ivan Nikitin <vanechka-nikitin-2004@mail.ru>
2023-02-19 21:06:22 +0800

    refactor: Add function 'get_absolute_url' in class 'Item'

--- project/products/models.py
+++ project/products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Item(models.Model):
@@ -8,3 +9,6 @@ class Item(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail-product', kwargs={'pk': self.pk})
