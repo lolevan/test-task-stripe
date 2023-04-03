b5b884c026fbd94a3af1bb3f40996037a46f6a39
Ivan Nikitin <vanechka-nikitin-2004@mail.ru>
2023-04-03 18:57:01 +0800

    Add library and fix entrypoint.sh

--- project/entrypoint.sh
+++ project/entrypoint.sh
@@ -6,17 +6,18 @@ then
     echo "DB not yet run..."
 
     # Проверяем доступность хоста и порта
-    while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
+    while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
       sleep 0.1
     done
 
     echo "DB did run."
 fi
-
 # Удаляем все старые данные
 python manage.py flush --no-input
-
 # Выполняем миграции
 python manage.py migrate
+
+# shellcheck disable=SC2145
+echo "Executing command: $@"
 
 exec "$@"
